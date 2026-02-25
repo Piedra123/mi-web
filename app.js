02f4708d1ba23b8fa66e03f68b2241f67ba6082d
Cristhopher E. Piedra Vargas <cepiedra@matra.cr>
2026-02-25 12:20:55 -0600

    Agregado app.js carrusel

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-document.getElementById("btn").addEventListener("click", () => {
-  alert("Bienvenido a Carwash 🚗");
-});
+// document.getElementById("btn").addEventListener("click", () => {
+//   alert("Bienvenido a Carwash 🚗");
+// });
+
+let index = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlide() {
+
+  slides.forEach(slide => slide.classList.remove("active"));
+
+  slides[index].classList.add("active");
+
+  index++;
+
+  if(index >= slides.length){
+    index = 0;
+  }
+}
+
+setInterval(showSlide, 3000);
+showSlide();
